658b69a4cdb16e819b5f0f7bab1c37850e12e19e
戸田晋央 <todasusumuou@macbook-air.local>
2020-04-30 05:14:28 +0900

    Initial Commit

---
+++ clasyfyEMG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clasyfyEMG
+//
+//  Created by 戸田晋央 on 2020/04/30.
+//  Copyright © 2020 戸田晋央. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
